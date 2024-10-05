63bd38543be2976631a65853b4e68af6a0316dd7
Albinoni <albinoni1423@gmail.com>
2024-10-05 10:44:42 -0300

    XeberDetailPage: ImageBox added

---
+++ src/pages/XeberDetailPage/Image/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import style from "./index.module.scss"
+import IconContainer from '../../../components/common components/SocialMediaIcons/IconContainer'
+
+function ImageBox({ image, children, date }) {
+  return (
+    <div className={style.container}>
+      <div className={style.imgBox}>
+        <img src={image} />
+        <h5 className={style.title}>{children}</h5>
+      </div>
+      <div className={style.infoBox}>
+        <IconContainer />
+        <span className={style.date}>{date}</span>
+      </div>
+    </div>
+  )
+}
+
+export default ImageBox
