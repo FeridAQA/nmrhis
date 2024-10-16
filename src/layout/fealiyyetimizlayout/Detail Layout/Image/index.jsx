2ce716b096fe6a0d8871ef17266eae55066f48ee
Albinoni <albinoni1423@gmail.com>
2024-10-16 01:39:54 -0300

    FealiyyetimizDetailPage fixed to responsive

--- src/layout/fealiyyetimizlayout/Detail Layout/Image/index.jsx
+++ src/layout/fealiyyetimizlayout/Detail Layout/Image/index.jsx
@@ -5,13 +5,14 @@ import IconContainer from '../../../../components/common components/SocialMediaI
 function ImageBox({ image, children, date }) {
   return (
     <div className={style.container}>
+      <h5 className={style.title} id={style.dekstop}>{children}</h5>
       <div className={style.imgBox}>
         <img src={image} />
         <h5 className={style.title}>{children}</h5>
       </div>
       <div className={style.infoBox}>
-        <IconContainer />
         <span className={style.date}>{date}</span>
+        <IconContainer />
       </div>
     </div>
   )
