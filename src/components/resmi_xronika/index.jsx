783cabf0d6bea99a98b3deac975e26902060a904
ferid aqa <feridcodee@gmail.com>
2024-11-09 21:20:18 +0400

    resmi xronika yazildi

--- src/components/resmi_xronika/index.jsx
+++ src/components/resmi_xronika/index.jsx
@@ -3,38 +3,114 @@ import './index.scss'
 
 import gerb from '../../assets/img/image.png'
 
-import ipone from '../../assets/img/remi_xronika/telefon.png'
 import foto from '../../assets/img/remi_xronika/image.png'
+import Iphone12 from '../common components/Iphone12'
 
+import InstagramIcon from '@mui/icons-material/Instagram';
+
+import FacebookIcon from '@mui/icons-material/Facebook';
+import { Link } from 'react-router-dom'
 
 function Resmi_xronika() {
     return (
-        <div id='resmi_xronika'>
-            <div className="gerb">
-                        <img src={gerb} alt="" />
-                    </div>
-            <div className="container">
-                <div className="title">
-                    <div className="text">
-                        Rəsmi xronika
-                    </div>
-                    <div className="text">
-                        Bizi izlə
-                    </div>
-                    
+        <>
+            <div id='resmi_xronika'>
+                <div className="gerb">
+                    <img src={gerb} alt="" />
                 </div>
-                <div className="down">
-                    <div className="imgbox">
-                        <div className="ipone">
-                            <img src={ipone} alt="" />
+                <div className="container">
+                    <div className="title">
+                        <div className="text">
+                            Rəsmi xronika
                         </div>
-                        <div className="img">
-                            <img src={foto} alt="" />
+                        <div className="text">
+                            Bizi izlə
+                        </div>
+
+                    </div>
+                    <div className="down">
+
+                        <div className="left">
+                            <Iphone12 image={foto}></Iphone12>
+                        </div>
+                        <div className="right">
+                            <div className="box1">
+                                <div className="border">
+                                    <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+                                        <div className="logo">
+                                            <InstagramIcon className='icon'></InstagramIcon>
+                                        </div>
+                                    </a>
+                                    <div className="text">
+                                        10B
+                                        İzləyici
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="box2">
+                                <div className="border">
+                                    <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+                                        <div className="logo">
+                                            <FacebookIcon className='icon'></FacebookIcon>
+                                        </div>
+                                    </a>
+                                    <div className="text">
+                                        787
+                                        İzləyici
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <div id='resmi_xron_mobil'>
+            <div className="gerb">
+                    <img src={gerb} alt="" />
+                </div>
+                <div className="container">
+
+                    <div className="text">
+                        Rəsmi xronika
+                    </div>
+                    <div className="left">
+                        <Iphone12 image={foto}></Iphone12>
+                    </div>
+                    <div className="text">
+                        Bizi izlə
+                    </div>
+                    <div className="right">
+                        <div className="box1">
+                            <div className="border">
+                                <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+                                    <div className="logo">
+                                        <InstagramIcon className='icon'></InstagramIcon>
+                                    </div>
+                                </a>
+                                <div className="text">
+                                    10B
+                                    İzləyici
+                                </div>
+                            </div>
+                        </div>
+                        <div className="box2">
+                            <div className="border">
+                                <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+                                    <div className="logo">
+                                        <FacebookIcon className='icon'></FacebookIcon>
+                                    </div>
+                                </a>
+                                <div className="text">
+                                    787
+                                    İzləyici
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+
     )
 }
 
