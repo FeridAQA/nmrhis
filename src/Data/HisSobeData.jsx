8cf1cfee83ed9327081010567d120ee52620825f
Albinoni <albinoni1423@gmail.com>
2024-11-10 10:34:51 -0300

    HisSobeData added

---
+++ src/Data/HisSobeData.jsx
@@ -0,0 +1,52 @@
+export default [
+  {
+    title: "TƏŞKİLATİ-METODİKİ ŞÖBƏ",
+    mudir: "Məmmədov Ceyhun İmran oğlu",
+    tel: "(036) 550 77 66 <br /> (070) 282 17 79",
+    email: "histeskilatisobe@gmail.com"
+  },
+  {
+    title: "Əməyin və sosial iqtisadi mənafelərin müdafiəsi şöbəsi",
+    mudir: "Əsgərov Eyvaz Mustafa oğlu",
+    tel: "(036) 550 47 44 <br /> (060) 210 11 68",
+  },
+  {
+    title: "Əməyin mühafizəsi şöbəsi",
+    mudir: "Katib, Xudaverdiyev Rəşadət Cabbar oğlu",
+    tel: "(036) 550 96 71 <br /> (050) 667 70 74",
+  },
+  {
+    title: "Maliyyə şöbəsi",
+    mudir: "Kələşov İmran Əmiraslan oğlu",
+    tel: "(036) 545 03 76 <br /> (060) 210 78 78 <br /> (070) 350 39 27",
+  },
+  {
+    title: " Ümumi işlər bölməsi",
+    mudir: "Əsədov Nüsrəddin Müseyib oğlu",
+    tel: "(050)456 75 00 <br /> (060) 456 75 00 <br /> (036) 545 74 26",
+    email: "his.umumiisler@gmail.com"
+  },
+  {
+    title: "HÜQUQ bölməsi",
+    mudir: "İsmayılov İlqar Cəlal oğlu",
+    tel: "(+994)559120050",
+    email: "levshailgar@mail.ru"
+  },
+  {
+    title: "Qəbul bölməsi",
+    mudir: "Xəlilov Rövnəq Şəmsəddin oğlu",
+    tel: "(060) 211 21 91 <br /> (050) 227 37 68 <br /> (036) 545 31 28",
+    email: "nmrhisqebul@gmail.com"
+  },
+  {
+    title: "Mətbuat və ictimaiyyətlə əlaqələr bölməsi",
+    mudir: "Ad yoxdur",
+    tel: "(060) 200 22 00",
+    email: "rahimlix2000@gmail.com"
+  },
+  {
+    title: "Təsərrüfat bölməsi",
+    mudir: "Əliyev Rəşid Məmməd oğlu",
+    tel: "(050) 498 52 72",
+  },
+]
