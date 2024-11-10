aa7d1cf867b8a18238473ab6a34079a44900322d
Albinoni <albinoni1423@gmail.com>
2024-11-10 10:11:34 -0300

    NavLink Active problem fixed

--- src/components/Subnav_iki/index.jsx
+++ src/components/Subnav_iki/index.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import "./Leadership.scss";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 
 const SubNavbar_iki = () => {
+
+  const location = useLocation();
+
   return (
     <div id="SubNavbar_iki">
       <div className="tab-menu">
         <NavLink to="/struktur/uzv_teskilatlar" className="tab-item">Üzv Təşkilatlar</NavLink>
-        <NavLink to="/struktur/tabe_teskilatlar/2" className="tab-item ">Tabe Təşkilatlar</NavLink>
+        <NavLink
+          to="/struktur/tabe_teskilatlar/:id"
+          className={({ isActive }) =>
+            isActive || location.pathname.startsWith("/struktur/tabe_teskilatlar")
+              ? "tab-item active"
+              : "tab-item"
+          }
+          isActive={() => location.pathname.startsWith("/struktur/tabe_teskilatlar")}>
+          Tabe Təşkilatlar
+        </NavLink>
         <NavLink to="/struktur/his_sobeleri" className="tab-item">HİŞ Şöbələri və Bölmələri</NavLink>
       </div>
     </div>
