1cad94a34c298d20440b3873531e636aa6d5a360
Albinoni <albinoni1423@gmail.com>
2024-11-10 04:13:35 -0300

    Tabe Teskilatlar added

--- src/Data/MekanData.jsx
+++ src/Data/MekanData.jsx
@@ -1,1 +1,138 @@
-export const MekanData = []
+export default [
+  {
+    id: 1,
+    title: "Ağbulaq İstirahət Mərkəzi",
+    image: "https://img.freepik.com/premium-photo/mountain-lake-with-reflection-trees-mountains-background_583952-93299.jpg",
+    info: [
+      {
+        title: "Ağbulaq İstirahət Mərkəzi",
+        context: "Ağbulaq İstirahət Kompleksi dəniz səviyyəsindən 2000 metr yüksəklikdə, Şahbuz rayonunun eyniadlı kəndində yerləşir. Ağbulaq Naxçıvan şəhərindən 55 km, Hava Limanından isə 60 km məsafədə yerləşir. Ağbulaq Kənd Turizm-İstirahət Bölgəsi 2018-ci ildə yaradılıb. <br /> 24 iyun 2013-cü il tarixdən etibarən Ağbulaq İstirahət Mərkəzi və Ağbulaq Xizək Mərkəzi Həmkarlar İttifaqları Şurası tərəfindən satın alınıb. Həmin tarixdən etibarən mərkəzdə yaşıllaşdırma və əsaslı bərpa işləri aparılır. <br /> Mərkəzdə ümumi tutumu 80 nəfər olan 38 otaq, 3 restoran, 2 əyləncə parkı fəaliyyət göstərir. <br /> Otaqlar peyk yayımlı TV, daxili xəttli telefon, mini bar, kassa, vanna otağı (duş, duş kabini), WC, fenlə təchiz olunub. <br /> İstirahət Mərkəzində Ağbulaq restoranı, kafe-bar, restoran-bar fəaliyyət göstərir. Bundan başqa mini futbol-voleybol meydanı, uşaqlar üçün əyləncə parkı, market, valyuta məntəqəsi, poçt xidməti və başqa xidmətlər də mövcuddur.",
+      },
+      {
+        title: "Ağbulaq İstirahət Mərkəzində Standart otaqda:",
+        context: "Tək çarpayı, oturma dəsti, LCD televizor, minibar, elektrikli çaydan, buklet dəsti, telefon xidməti, şkaf, paltar asılqanı, wifi, mərkəzi isitmə sistemi, sabun, şampun, duş jeli, xalat, dəsmal, fen, 24 saat otaq xidməti, çamaşırxana xidməti, fərdi ütü servisi, çay-kofe xidməti mövcuddur.",
+      },
+      {
+        title: "Tək nəfərlik otaqda:",
+        context: "İki çarpayı <br /> LCD televizor <br /> Mərkəzi kondisioner sistemi <br /> Saçqurudan fen <br /> Mini bar <br /> Elektron seyf <br /> WİFİ <br /> Yanğın siqnalizasiya sistemi <br /> 24 saat otaq xidməti",
+      },
+      {
+        title: "Cüt nəfərlik otaqda:",
+        context: "İki çarpayı <br /> LCD televizor <br /> Mərkəzi kondisioner sistemi <br /> Saçqurudan fen <br /> Mini bar <br /> Elektron seyf <br /> WİFİ <br /> Yanğın siqnalizasiya sistemi <br /> 24 saat otaq xidməti",
+      },
+      {
+        title: "Xidmətlər (TWİN DOUBLE)",
+        context: "24 saat Qəbul vəYerləşdirmə <br /> 24 saat Restoran və Bar <br /> İlk tibbi yardim otağı <br /> 24 saat Otaq xidməti <br /> 24 saat Təhlükəsizlik xidməti <br /> Pulsuz Park etmə <br /> Oyandırma xidməti <br /> Bələdçi ( ödənişli ) <br /> Yanğınsöndürən",
+      },
+      {
+        title: "RESTORANLAR",
+        context: "Ağbulaq İstirahət Mərkəzində 48 nəfər tutumlu əsas restoran , 18 nəfər tutumlu yumşaq mebellərlə təchiz olunmuş “Salon” adlanan restoran və Böyük zal sizə 24 saat xidmət göstərir. Ağbulaq İstirahət Mərkəzinin restoranında 48 nəfər tutumlu əsas restoran, 18 nəfər tutumlu yumşaq mebellərlə təchiz olunmuş “Salon” adlanan restoran 24 saat xidmət göstərir. Həmçinin bar bölümündən müxtəlif növ içkilər, kənd üsulu hazırlanmış kompotlar, şərbətlər və s. tipli içkilər mövcuddur. <br /> Xidmətlər: <br /> 24 saat Qəbul və Yerləşdirmə <br /> 24 saat Restoran və Bar <br />  İlk tibbi yardim otağı <br /> 24 saat Otaq xidməti <br /> 24 saat Təhlükəsizlik xidməti <br /> Pulsuz Park etmə <br /> Oyandırma xidməti <br /> Bələdçi ( ödənişli ) <br /> Yanğınsöndürən",
+      },
+      {
+        title: "AĞBULAQ XİZƏK MƏRKƏZİ",
+        context: "2018-ci il 29 noyabr tarixli Sərəncam ilə Ağbulaq Kənd Turizm- İstirahət Bölgəsi yaradılıb. 2021-ci il Fevralın 17-də Ağbulaq Xizək Mərkəzi istifadəyə verilib. <br /> Dəniz səviyyəsindən 2 min metr yüksəklikdə yerləşən Xizək Mərkəzinin ümumi uzunluğu 1600, eni isə 30 metrdir. Mərkəzdə dağ- xizək enişi, kanat yolu, kafelər, süni qaryaratma sistemləri, xizəksürmə məktəbi, əyləncə və uşaq parkları, 20 min kubmetr tutumu olan süni göl və qaryaratma sistemləri quraşdırılıb. Mərkəzin əsas infrastrukturu kanat yoludur. 26 sabit, 2 hərəkət edən süni qar püskürtmə aparatı vasitəsilə ərazidə süni qar örtüyü yaradılır.",
+      },
+      {
+        title: "KANAT",
+        context: "Uzunluğu 952 metr <br /> Hündürlük fərqi 267 metr <br /> Tutumu saatda 1000 nəfər <br /> 58 ədəd 4 nəfərlik oturacaq",
+      },
+      {
+        title: "GEYİMLƏR VƏ LƏVAZİMATLAR",
+        context: "Mərkəzdə 120 dəst hər yaş qrupuna uyğun geyim və xizək sürənlər üçün də xüsusi geyim dəstləri mövcuddur. <br /> Burada turistlər rahat şəkildə geyimləri və ləvazimatları – gödəkcə, snoubord, kirşə, xizək ayaqqabısı, xizək çubuğu, xizək, dəbilqə, eynək, əlcək və şalvarı ən ucuz qiymətə kirayə edə bilirlər. Mərkəzdə ixtisaslı təlimçilər müxtəlif hazırlıq səviyyəsi olan xizəkçilər üçün təlimlər keçir və təhlükəsizlik qaydalarını izah edirlər.",
+      },
+      {
+        title: "GEYİMLƏR VƏ AVADANLIQLARIN KİRAYƏSİ XİZƏK KiRAYƏSİ",
+        context: "Ağbulaq Xizək Mərkəzi öyrənməni sürətləndirmək üçün yaradılmış xüsusi xizək avadanlıqlarının- xizək və xizək çubuğu, xizək ayaqqabısı, gödəkçə, şalvar, snoubordun kirayəsi mövcuddur.",
+      },
+      {
+        title: "SNOWBOARD KİRAYƏSİ",
+        context: "Ağbulaq Xizək Mərkəzi qar və ərazi şəraitinə uyğun olaraq yüksək keyfiyyətli və K2, öyrənmə prosesini sürətləndirən avadanlıqlar təklif edir.",
+      },
+      {
+        title: "XİZƏK MƏKTƏBİ YENİ BAŞLAYANLAR ÜÇÜN TƏLİMLƏR",
+        context: "Ağbulaq xizək məktəbində Türkiyədən dəvət olunmuş ixtisaslı təlimçilər müxtəlif hazırlıq səviyyəsi olan xizəkçilər üçün təlimlər həyata keçirir və təhlükəsizliyin təmin edilməsini öyrədirlər. <br /> İlkin öyrənmə metodu <br /> İlkin öyrənənlər üçün xüsusi sürüşmə zonası mövcuddur. Təlimçilər xizəklə sürüşmənin əsas fəlsəfəsini, ayaqüstə dayana bilmə və s. priyomları öyrətdikdən sonra, sürüşmə zamanı qarşıya çıxan çətinlikləri və sürüşmə zamanı yalnış qərarvermələri aradan qaldırırlar.",
+      },
+      {
+        title: "KAFE",
+        context: "Ağbulaq Xizək Mərkəzində 2 kafe-bar fəaliyyət göstərir. Kanat liftinin başlanğıcında və sonunda fəaliyyət göstərən kafelərdə qonaqlara tez hazırlanan qida və içkilər təqdim olunur. Kofe, çay və digər içki çeşidləri ilə zəngindir.",
+      },
+      {
+        title: "ƏYLƏNCƏ PARKI",
+        context: "Uzunluğu 145 metr <br /> Eni 40 metr <br /> Sahəsi 5855 m2 <br /> KONVEYOR XƏTTİ <br /> Əyləncə parkı 145 metr <br /> Uşaq parkı 32 metr <br /> Başlanğıc sahə 53 metr",
+      },
+      {
+        title: "UŞAQ PARKI",
+        context: "Uzunluğu 65 metr <br /> Eni 25 metr <br /> Sahəsi 1630 m2",
+      },
+      {
+        title: "Əlaqə",
+        context: "Bizimlə əlaqə yaradın <br /> Ünvan: Naxçıvan MR. Şahbuz rayonu Ağbulaq kəndi <br /> Tel: +994602040054 <br /> E-mail: info@agbulaq.com <br /> İnstagram: agbulaq_ski_center <br /> Facebook: agbulaqxizekmerkezi",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "“Darıdağ Arsenli Su Müalicə Mərkəzi” MMC",
+    image: "https://plus.unsplash.com/premium_photo-1669295395768-6ef852fddc90?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bmF0dXJlJTIwd2FsbHBhcGVyfGVufDB8fDB8fHww",
+    info: [
+      {
+        title: "“Darıdağ Arsenli Su Müalicə Mərkəzi” MMC",
+        context: "Darıdağ Arsenli Su Müalicə Mərkəzi” MMC Culfa şəhərindən 8-9 kilometr şimal-şərqdə, Darıdağın sıldırım yamacları arasında, dəniz səviyyəsindən 800-900 metrhündürlükdə yerləşir. 2 avqust 2023-cü il tarixdən etibarən isə “Darıdağ Arsenli Su Müalicə Mərkəzi” MMC Naxçıvan Muxtar Respublikası Həmkarlar İttifaqları Şurasının balansında fəaliyyət göstərməyə başlayıb. <br /> “Darıdağ” suyu karbon qazlı, mərgümüşlü, yüksək minerallaşdırılmış xlorlu-hidrokarbonatlı-natriumludur: Kudova (Polşa), Dürkgeym (Almaniya), Sineqorsk (Saxalin) sularının analoqudur. Lakin suyun tərkibindəki mineral duzların mütənasibliyinə görə “Darıdağ” suyu onlardan üstündür. “Darıdağ” suyundan hələ qədim zamanlardan kustar şəkildə sümük-oynaq, əzələ sistemi xəstəliklərinin, o cümlədən “duzlaşma” adlanan patologiyalar və onun müxtəlif fəsadlarının müalicəsində geniş istifadə edilib. Hazırda burada arsenli su vannası vasitəsilə revmatik, dəri, əzələ və sinir sistemləri xəstəliklərinin ambulator müalicəsi aparılır. Bu mineral sudan istər soyuq istərsə də isti içmək zərərlidir.",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Darıdağ Arsenli Su Müalicə və İstirahət Kompleksi",
+    image: "https://static.vecteezy.com/system/resources/previews/032/259/695/large_2x/beautiful-peacock-in-the-water-flowers-house-nature-wallpaper-hd-wallpaper-ai-generated-free-photo.jpg",
+    info: [
+      {
+        title: "Darıdağ Arsenli Su Müalicə və İstirahət Kompleksi",
+        context: "Darıdağ Arsenli Su Müalicə və İstirahət Kompleksi həmkarlar ittifaqının balansına alındığı vaxtdan bu günədək quruculuq işləri davam etdirilir. Hotelin içində yerləşməsi nəzərdə tutulan qazanxana üçün yeni bina tikilir. Elektrik otaqları və yeni su hovuzları yaradılır. Həmçinin binalar arasında mövcud olan 250 metrlik kommunikasiya tuneli standartlara uyğun olmadığından genişləndirilir.",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "“Duzdağ” Hotel",
+    image: "https://static.vecteezy.com/system/resources/previews/032/266/838/large_2x/wallpaper-water-flowers-house-nature-nature-wallpaper-wallpaper-hd-nature-wallpaper-ai-generated-free-photo.jpg",
+    info: [
+      {
+        title: "“Duzdağ” Hotel",
+        context: "2008-ci ildən bəri fəaliyyət göstərən və hazırda ölkənin ən yaxşı istirahət mərkəzlərindən biri olan “Duzdağ” istirahət kompleksi, ləziz və sağlam mətbəxi, bir-birindən maraqlı əyləncə növləri, geniş və işıqlı otaqları ilə 5 ulduzlu məkanın lüks və rahatlığını özündə birləşdirir. “Duzdağ” hoteldə 100 standart, 35 kiçik suit, 8 suit, 1 kral dairəsi olmaqla 144 otaq və 7 villa fəaliyyət göstərir. Hoteldə Aqua park, sürüş zolağı, oyun zalı və kafedən ibarət Duzdağ Kartinq Mərkəzi, Duzdağ Hotel Spa Mərkəzi, mini futbol meydançası da var.",
+      },
+      {
+        title: "“Duzdağ” hotel otaqları Standart",
+        context: "Sahəsi 28 m2 standart otağa təkçarpayı, bir ədəd oturma dəsti, LCD televizor, mini bar, elektrikli çaydan, elektron seyf, telefon xidməti, şkaf, patlar asılqanı, qiblə göstəricisi, qeyd kitabçası, qaralama kağızlar, kataloqlar, xalça döşəmə daxildir.",
+      },
+      {
+        title: "Juniorsuite",
+        context: "Sahəsi 42 m2 juniorsuite otağa orta geniş zal, təkçarpayı, oturma dəsti, LCD televizor, mini bar, elektrikli çaydan, elektron seyf, telefonxidməti, şkaf, paltarasılqanı, qibləgöstəricisi, qeydkitabçası, qaralamakağızlar, kataloqlar, xalça döşəmə daxildir.",
+      },
+      {
+        title: "Kral dairəsi",
+        context: "Həyətyanı sahədə 7 ədəd kottec mövcuddur. Sahəsi 90 m2 olan kotteclərin hər biri 3 otaqdan ibarətdir. Kotteclərdə double və twin yataqlar, iki yataq otağı və geniş zal mövcuddur.",
+      },
+      {
+        title: "Kottec",
+        context: "Həyətyanı sahədə 7 ədəd kottec mövcuddur. Sahəsi 90 m2 olan kotteclərin hər biri 3 otaqdan ibarətdir. Kotteclərdə double və twin yataqlar, iki yataq otağı və geniş zal mövcuddur.",
+      },
+      {
+        title: "“Duzdağ” Hotel Spa Mərkəzi",
+        context: "SPA mərkəzimizdə sauna otağı, buxar və türk hamamı, qapalı hovuzlar, masaj terapiyası, fitness və aerobika mərkəzi mövcuddur.",
+      },
+      {
+        title: "Restoran",
+        context: "Hotelin restoranında 24 saat xidmət göstərən Açıq Bufet, Canlı musiqi, Kafe Bar fəaliyyət göstərir.",
+      },
+      {
+        title: "Tədbir zalı",
+        context: "“Duzdağ” hoteldə xüsusi dövlət tədbirləri, iclaslar, seminar və təlimlər üçün nəzərdə tutulmuş iclas zalı, 400 nəfərədək tutumu olan Banket zalı da fəaliyyət göstərir.",
+      },
+      {
+        title: "Fizioterapiya Mərkəzi",
+        context: "“Duzdağ” hotelinin daxilində yerləşən fizioterapiya mərkəzində dərman bitkiləri və efir yağları ilə müalicəvi vannalar, müalicəvi masaj, darıdağ vannaları, şor su yatağından palçıq müalicəsi təcrübəli həkim və tibb bacıları tərəfindən həyata keçirilir.",
+      },
+    ],
+  },
+]
