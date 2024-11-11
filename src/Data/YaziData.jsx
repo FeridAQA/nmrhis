14c655f6bee7e6be9bfb4586ad026b4d9b61ded3
Albinoni <albinoni1423@gmail.com>
2024-11-11 16:38:17 -0300

    BizeYazirlarPage: ready

--- src/Data/YaziData.jsx
+++ src/Data/YaziData.jsx
@@ -1,22 +1,27 @@
 export default [
   {
     writer: "Həmidə Məmmədova",
-    content: "Hörmətli Naxçıvan Muxtar Respublikası Həmkarlar İttifaqları Şurası! Əvvəlcə Şuranın səmərəli işlərinə görə minnətdarlıq edirəm. Mən, 2022-ci il üzrə təhsil sahəsi üzrə 'İlin peşəkar gənci' fəxri adına və mükafatına layiq görülmüşdüm. Biz fəal gənclərin əməyinə diqqətlə yanaşdığınıza görə peşəkar və səmimi kollektivinizə təşəkkürlərimi bildirirəm"
+    content: "Hörmətli Naxçıvan Muxtar Respublikası Həmkarlar İttifaqları Şurası! Əvvəlcə Şuranın səmərəli işlərinə görə minnətdarlıq edirəm. Mən, 2022-ci il üzrə təhsil sahəsi üzrə 'İlin peşəkar gənci' fəxri adına və mükafatına layiq görülmüşdüm. Biz fəal gənclərin əməyinə diqqətlə yanaşdığınıza görə peşəkar və səmimi kollektivinizə təşəkkürlərimi bildirirəm",
+    date: "20 Sentyabr 2024",
   },
   {
     writer: "Vaqif Esedov",
-    content: "Hörmətli Naxçıvan Muxtar Respublikası Həmkarlar İttifaqları Şurası! Əvvəlcə Şuranın səmərəli işlərinə görə minnətdarlıq edirəm. Mən, 2022-ci il üzrə təhsil sahəsi üzrə 'İlin peşəkar gənci' fəxri adına və mükafatına layiq görülmüşdüm. Biz fəal gənclərin əməyinə diqqətlə yanaşdığınıza görə peşəkar və səmimi kollektivinizə təşəkkürlərimi bildirirəm"
+    content: "Hörmətli Naxçıvan Muxtar Respublikası Həmkarlar İttifaqları Şurası! Əvvəlcə Şuranın səmərəli işlərinə görə minnətdarlıq edirəm. Mən, 2022-ci il üzrə təhsil sahəsi üzrə 'İlin peşəkar gənci' fəxri adına və mükafatına layiq görülmüşdüm. Biz fəal gənclərin əməyinə diqqətlə yanaşdığınıza görə peşəkar və səmimi kollektivinizə təşəkkürlərimi bildirirəm",
+    date: "20 Sentyabr 2024",
   },
   {
     writer: "Həmidə Məmmədova",
-    content: "Hörmətli Naxçıvan Muxtar Respublikası Həmkarlar İttifaqları Şurası! Əvvəlcə Şuranın səmərəli işlərinə görə minnətdarlıq edirəm. Mən, 2022-ci il üzrə təhsil sahəsi üzrə 'İlin peşəkar gənci' fəxri adına və mükafatına layiq görülmüşdüm. Biz fəal gənclərin əməyinə diqqətlə yanaşdığınıza görə peşəkar və səmimi kollektivinizə təşəkkürlərimi bildirirəm"
+    content: "Hörmətli Naxçıvan Muxtar Respublikası Həmkarlar İttifaqları Şurası! Əvvəlcə Şuranın səmərəli işlərinə görə minnətdarlıq edirəm. Mən, 2022-ci il üzrə təhsil sahəsi üzrə 'İlin peşəkar gənci' fəxri adına və mükafatına layiq görülmüşdüm. Biz fəal gənclərin əməyinə diqqətlə yanaşdığınıza görə peşəkar və səmimi kollektivinizə təşəkkürlərimi bildirirəm",
+    date: "20 Sentyabr 2024",
   },
   {
     writer: "Kenan Aslanli",
-    content: "Hörmətli Naxçıvan Muxtar Respublikası Həmkarlar İttifaqları Şurası! Əvvəlcə Şuranın səmərəli işlərinə görə minnətdarlıq edirəm. Mən, 2022-ci il üzrə təhsil sahəsi üzrə 'İlin peşəkar gənci' fəxri adına və mükafatına layiq görülmüşdüm. Biz fəal gənclərin əməyinə diqqətlə yanaşdığınıza görə peşəkar və səmimi kollektivinizə təşəkkürlərimi bildirirəm"
+    content: "Hörmətli Naxçıvan Muxtar Respublikası Həmkarlar İttifaqları Şurası! Əvvəlcə Şuranın səmərəli işlərinə görə minnətdarlıq edirəm. Mən, 2022-ci il üzrə təhsil sahəsi üzrə 'İlin peşəkar gənci' fəxri adına və mükafatına layiq görülmüşdüm. Biz fəal gənclərin əməyinə diqqətlə yanaşdığınıza görə peşəkar və səmimi kollektivinizə təşəkkürlərimi bildirirəm",
+    date: "20 Sentyabr 2024",
   },
   {
     writer: "Vaqif Esedov",
-    content: "Hörmətli Naxçıvan Muxtar Respublikası Həmkarlar İttifaqları Şurası! Əvvəlcə Şuranın səmərəli işlərinə görə minnətdarlıq edirəm. Mən, 2022-ci il üzrə təhsil sahəsi üzrə 'İlin peşəkar gənci' fəxri adına və mükafatına layiq görülmüşdüm. Biz fəal gənclərin əməyinə diqqətlə yanaşdığınıza görə peşəkar və səmimi kollektivinizə təşəkkürlərimi bildirirəm"
+    content: "Hörmətli Naxçıvan Muxtar Respublikası Həmkarlar İttifaqları Şurası! Əvvəlcə Şuranın səmərəli işlərinə görə minnətdarlıq edirəm. Mən, 2022-ci il üzrə təhsil sahəsi üzrə 'İlin peşəkar gənci' fəxri adına və mükafatına layiq görülmüşdüm. Biz fəal gənclərin əməyinə diqqətlə yanaşdığınıza görə peşəkar və səmimi kollektivinizə təşəkkürlərimi bildirirəm",
+    date: "20 Sentyabr 2024",
   },
 ]
--- src/pages/BizeYazirlar/YaziCard/index.jsx
+++ src/pages/BizeYazirlar/YaziCard/index.jsx
@@ -2,10 +2,20 @@ import React from 'react'
 import style from "./index.module.scss"
 
 function YaziCard(props) {
+
   return (
     <div className={style.container}>
-      <h3 className={style.writer}>{props.writer}</h3>
-      <p className={style.content}>{props.content}</p>
+      <h3
+        className={`${style.writer} ${props.isFirstSlide ? style.firstSlide : ""}`}
+      >
+        {props.writer}
+      </h3>
+      <p
+        className={`${style.content} ${props.isFirstSlide ? style.firstSlide : ""}`}
+      >
+        {props.content}
+      </p>
+      <span className={style.date}>{props.date}</span>
     </div>
   )
 }
