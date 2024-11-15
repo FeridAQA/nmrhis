5993d012333fef93ef4a6cc266ae51ee864573c1
Albinoni <albinoni1423@gmail.com>
2024-11-15 11:28:24 -0300

    XeberDeatil: ImageSlider added

--- src/pages/XeberDetailPage/Image/index.jsx
+++ src/pages/XeberDetailPage/Image/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import style from "./index.module.scss"
 import IconContainer from '../../../components/common components/SocialMediaIcons/IconContainer'
+import ImageSlider from './ImageSlider'
 
-function ImageBox({ image, children, date }) {
+function ImageBox({ images, children, date }) {
   return (
     <div className={style.container}>
       <div className={style.imgBox}>
-        <img src={image} />
+        <ImageSlider images={images} />
         <h5 className={style.title}>{children}</h5>
       </div>
       <div className={style.infoBox}>
