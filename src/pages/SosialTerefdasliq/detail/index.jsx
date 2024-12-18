a55bf1bde5e4af116b17da91e9b6a381c0b56efc
Albinoni <albinoni1423@gmail.com>
2024-12-18 08:35:42 -0300

    SosialTerefdasliq: overflow fixed

--- src/pages/SosialTerefdasliq/detail/index.jsx
+++ src/pages/SosialTerefdasliq/detail/index.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import PageTitle from '../../../components/common components/PageTitle'
 import STDContainer from '../../../layout/SosialTerefdasliq/Container'
 import { Helmet } from 'react-helmet-async'
+import style from "./index.module.scss"
 
 function SosialTerefdasliqDetail() {
   return (
-    <div>
+    <div className={style.container}>
       <Helmet>
         <title>Sosial Tərəfdaşlıq - HİŞ </title>
       </Helmet>
